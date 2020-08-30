35ade266ab393e2141cc987e955169371d561e42
unknown <yyudie@gmail.com>
2020-08-30 15:22:06 +0700

    first initial

---
+++ index.js
@@ -0,0 +1,17 @@
+//const 
+/*
+bot.start((ctx)=> {                                                                                                               //
+    ctx.replyWithPhoto(                                                                                                             //
+        {                                                                                                                           //
+            source: "PANGSUMA.jpg"                                                                                                  //
+        },                                                                                                                          //
+    {                                                                                                                               //
+        reply_markup: {                                                                                                             //
+            inline_keyboard : [                                                                                                     //
+                [{text: "MULAI", callback_data: "mulai"}]                                                                           //
+            ]                                                                                                                       //
+        }                                                                                                                           //
+    })                                                                                                                              //                                                                                                                                    //
+}) 
+*/
+module.exports = ({ reply }) => reply('42')
