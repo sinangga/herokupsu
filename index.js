30ff8b3fb749f179ddf7963539af48d4013f6045
Neptusun <70463907+sinangga@users.noreply.github.com>
2020-10-30 11:26:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const bot = new Composer()
 const download = require('download')
 
 var d = new Date();
-
+var yy = d.getUTCFullYear()
+var yy = yy.toString ()
 if (d.getUTCMonth() <= 8) {
 	var mm = 1+d.getUTCMonth()
-	var mm = "0"+mm;  
+	var mm = "0"+mm
+  var mm = mm.toString ();
 } else {
-  	var mm = 1+d.getUTCMonth();
+  	var mm = 1+d.getUTCMonth()
+    var mm = mm.toString ();
 }
 
 var jam = d.getUTCHours()
@@ -19,8 +22,7 @@ if (jam < 12 ) {
     jam = "070000"
 }
 
-var tgl = mm + d.getUTCDate() + jam;
-
+var tgl = yy + mm + d.getUTCDate() + jam;
 /////////////////////////////////////////////////////////////////////
 //                                                                 //
 //              HEADER BOT TELEGRAM                                //
